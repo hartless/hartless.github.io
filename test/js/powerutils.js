adb370b27082e7d9ed9551ddcfe8afe52362cde8
hartless <behart@gmail.com>
2019-02-09 16:14:57 -0500

    clear out junk

--- test/js/powerutils.js
+++
@@ -1,5 +0,0 @@
-// IMPORTED VIA http://cdn.ey.com/echannel/gl/en/industries/power-utilities/scrollpage/js/powerutils.js
-// UPDATE BEFORE PUBLISHING
-    $(document).ready(function() {
-        
-    });
